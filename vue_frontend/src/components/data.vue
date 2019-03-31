846b275ec80239a2b37418d5c7e8c5d3f71dac6c
燕涛 <15243650315@163.com>
2019-03-31 15:36:30 +0800

    first commit

---
+++ vue_frontend/src/components/data.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-table
+    :data="tableData"
+    style="width: 100%">
+    <el-table-column
+      prop="date"
+      label="日期"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="name"
+      label="姓名"
+      width="180">
+    </el-table-column>
+    <el-table-column
+      prop="address"
+      label="地址">
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      tableData: []
+    }
+  },
+  created() {
+    axios.get('http://192.168.1.1/dataserver/').then(response => {
+      this.tableData = response.data
+    })
+  }
+}
+</script>
